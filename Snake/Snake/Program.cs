3d609f3eb747bc8933e7e4016815eab2b14be7a5
system10 <system10@inbox.ru>
2018-11-16 14:58:55 +0300

    Classes

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -13,14 +13,15 @@ namespace Snake
             int x1 = 1;
             int y1 = 2;
             char sym1 = '*';
-            Draw(x1, y1, sym1);
+            Point p1 = new Point();
+            p1.x = 1;
+            p1.y = 2;
+            p1.sym = '*';
+            p1.Draw();
+            
             Console.ReadLine();
         }
 
-        static void Draw (int x, int y, char sym)
-        {
-            Console.SetCursorPosition(x, y);
-            Console.Write(sym);
-        }
+        
     }
 }
